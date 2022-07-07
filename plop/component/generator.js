46cf557ee72686ffae193f84db66bb11a987c10d
s2junn <sejun.hwang@navercorp.com>
2022-07-07 17:33:02 +0900

    change a plop template

--- plop/component/generator.js
+++ plop/component/generator.js
@@ -3,25 +3,20 @@ module.exports = {
     prompts: [
         {
             type: "input",
-            name: "ComponentName",
-            message: "Component Name"
-        },
-        {
-            type: "input",
-            name: "ClassName",
-            message: "Class Name"
+            name: "name",
+            message: "Component Name: "
         }
     ],
     actions: function(data) {
         let actions = [
             {
                 type: "add",
-                path: "{{ properCase ComponentName }}/index.tsx",
+                path: "{{ dashCase name }}/index.tsx",
                 templateFile: "./plop/component/templates/ReactFunctionComponent.tsx.hbs"
             },
             {
                 type: "add",
-                path: "{{ properCase ComponentName }}/{{ComponentName}}.scss",
+                path: "{{ dashCase name }}/{{ dashCase name }}.scss",
                 templateFile: "./plop/component/templates/ReactFunctionComponent.scss.hbs"
             }
         ]
