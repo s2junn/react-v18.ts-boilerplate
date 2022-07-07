1ad023e3544a27e7688c6289cb425d1864b7f7bf
s2junn <sejun.hwang@navercorp.com>
2022-07-07 17:51:54 +0900

    storybook 에 scss 적용 및 component source 변경

--- src/stories/Header.stories.tsx
+++ src/stories/Header.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import { Header } from './Header';
+import { Header } from '../components/header';
 
 export default {
   title: 'Example/Header',
