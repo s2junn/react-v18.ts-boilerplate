f86e5ee0add7fdccbd2b967c9443e332e7b4259f
s2junn <sejun.hwang@navercorp.com>
2022-07-11 13:00:14 +0900

    fixed import errors

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import './button.scss';
+// import './button.scss';
+import { css, jsx } from '@emotion/react'
 
 export interface ButtonProps {
   primary?: boolean;
@@ -23,6 +24,7 @@ function Button ({
     <button
       type="button"
       className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+      css={ css`${button_css}` }
       style={{ backgroundColor }}
       {...props}
     >
@@ -34,3 +36,37 @@ function Button ({
 Button.defaultProps = {};
 
 export default React.memo( Button )
+
+const button_css = css`
+/* &.storybook-button { */
+  font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  font-weight: 700;
+  border: 0;
+  border-radius: 3em;
+  cursor: pointer;
+  display: inline-block;
+  line-height: 1;
+
+  &--primary {
+    color: white;
+    background-color: #1ea7fd;
+  }
+  &--secondary {
+    color: #333;
+    background-color: transparent;
+    box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+  }
+  &--small {
+    font-size: 12px;
+    padding: 10px 16px;
+  }
+  &--medium {
+    font-size: 14px;
+    padding: 11px 20px;
+  }
+  &--large {
+    font-size: 16px;
+    padding: 12px 24px;
+  }
+/* } */
+`
