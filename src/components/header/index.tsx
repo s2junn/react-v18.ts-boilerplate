18e7a7814d2505aa5c06a970ee0892144b38bc4c
s2junn <sejun.hwang@navercorp.com>
2022-07-12 13:04:11 +0900

    changed folder structure for atomic design patterns

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Button from '../button'
+import Button from '../../atoms/button'
 import './header.scss'
 
 type User = {
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,7 @@
-import Button from './button'
-import Header from './header'
-import Layout from './layout'
-import Navigation from './navigation'
-import Page from './page'
+import Button from './atoms/button'
+import Navigation from './molecules/navigation'
+import Header from './organisms/header'
+import Layout from './templates/layout'
+import Page from './templates/page'
 
 export { Button, Header, Layout, Navigation, Page }
